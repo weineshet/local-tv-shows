2bb1b14d7322d541147368784ec061dffce076e7
weineshet <52981354+weineshet@users.noreply.github.com>
2019-09-18 21:49:04 -0700

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,29 @@ import { AppComponent } from './app.component';
 import { CurrentTvshowComponent } from './current-tvshow/current-tvshow.component';
 import {TvshowService} from './tvshow/tvshow.service';
 import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule, MatFormFieldModule, MatInputModule} from'@angular/material';
+import { CitySearchComponent } from './city-search/city-search.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    CurrentTvshowComponent
+    CurrentTvshowComponent,
+    CitySearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [TvshowService],
   bootstrap: [AppComponent]
